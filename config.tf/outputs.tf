4cfb70944bddc41ec383010f47107435fdf28a21
Evgenii-379 <152536714+Evgenii-379@users.noreply.github.com>
2025-04-23 14:39:11 +0300

    Create outputs.tf

---
+++ config.tf/outputs.tf
@@ -0,0 +1,12 @@
+output "bucket_url" {
+  description = "Публичная ссылка на картинку из Object Storage"
+  value       = "https://${var.bucket_name}.storage.yandexcloud.net/image.jpg"
+}
+
+output "nlb_ip" {
+  description = "Внешний IP адрес сетевого балансировщика (NLB)"
+  value = tolist([
+    for listener in yandex_lb_network_load_balancer.nlb.listener :
+    tolist([for spec in listener.external_address_spec : spec.address])[0]
+  ])[0]
+}
